a52c67a24cc9274fcda992e0b0b3a69ea65f9175
SoldatdOR05 <epaleyron@edenschool.fr>
2023-05-05 13:43:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const blue = document.getElementById('b');
+const green = document.getElementById('g');
+const red = document.getElementById('r');
+const yellow = document.getElementById('y');
+
+let table = [blue, green, red, yellow];
+
+for (let i = 0; i < table.length; i++) {
+  
+}
